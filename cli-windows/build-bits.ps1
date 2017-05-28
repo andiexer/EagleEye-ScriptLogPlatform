3b6f09023917441221c3e3d94bd0572238338c78
Andi Exer <andi.exer@windowslive.com>
2017-05-28 13:09:32 +0200

    finally add build bits for windows

---
+++ cli-windows/build-bits.ps1
@@ -0,0 +1,20 @@
+﻿Param(
+    [string]$rootPath,
+    [string]$SolutionFile = 'EESLP-DockerBITSOnly.sln'
+)
+
+$scriptPath = Split-Path $script:MyInvocation.MyCommand.Path
+
+if([string]::IsNullOrEmpty($rootPath)) {
+    $rootPath = "$scriptPath\.."
+}
+
+$SolutionFilePath = [IO.Path]::Combine($rootPath, $SolutionFile)
+
+Write-Host "Using Solution file '$SolutionFile' at Path $SolutionFilePath" -ForegroundColor Yellow
+
+# restore files
+dotnet restore $SolutionFilePath
+
+# publish files to specific docker obj output
+dotnet publish $SolutionFilePath -c Release -o .\obj\Docker\publish
