8e516783bc2ed74d5d054d8b36649aa7d9aa7424
Andi Exer <andi.exer@windowslive.com>
2017-05-29 09:31:22 +0200

    added custom mysql docker container with init data (atm schema only, demo data in progress) updated docker-compose for simple build and run updated remove-dockerimages file which also deletes all running containers called eeslp* added sql database model (mysql workbench required)

--- cli-windows/remove-dockerimages.ps1
+++ cli-windows/remove-dockerimages.ps1
@@ -3,6 +3,11 @@ $imagesToDelete = docker images --filter=reference="eeslp/*" -q
 If (-Not $imagesToDelete) {Write-Host "Not deleting eeslp images as there are no eeslp images in the current local Docker repo."} 
 Else 
 {
+    # Delete all containers
+    Write-Host "Deleting all containers in local Docker Host"
+    docker rm $(docker ps -a --filter "name=eeslp" -q) -f
+	
+	
     # Delete all eeslp images
     Write-Host "Deleting eeslp images in local Docker repo"
     Write-Host $imagesToDelete
---
+++ src/Sql/init.sql
@@ -0,0 +1,51 @@
+CREATE TABLE IF NOT EXISTS `EESLP`.`Host` (
+  `Id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `Hostname` VARCHAR(50) NOT NULL,
+  `FQDN` VARCHAR(250) NOT NULL,
+  `ApiKey` VARCHAR(250) NOT NULL,
+  `CreatedDateTime` DATETIME NULL,
+  `LastModDateTime` DATETIME NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE INDEX `FQDN_UNIQUE` (`FQDN` ASC),
+  INDEX `ApiKey_INDEX` (`ApiKey` ASC))
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `EESLP`.`Script` (
+  `Id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `Scriptname` VARCHAR(50) NOT NULL,
+  `Description` TEXT NULL,
+  `CreatedDateTime` DATETIME NULL,
+  `LastModDateTime` DATETIME NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE INDEX `ScriptName_UNIQUE` (`Scriptname` ASC))
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `EESLP`.`ScriptInstance` (
+  `Id` INT UNSIGNED NOT NULL,
+  `HostId` INT NOT NULL,
+  `ScriptId` INT NOT NULL,
+  `InstanceStatus` VARCHAR(45) NOT NULL,
+  `CreatedDateTime` DATETIME NOT NULL,
+  `EndDateTime` DATETIME NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE INDEX `Id_UNIQUE` (`Id` ASC),
+  INDEX `Status_INDEX` (`InstanceStatus` ASC))
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `EESLP`.`Log` (
+  `Id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `ScriptInstanceId` INT UNSIGNED NOT NULL,
+  `LogDateTime` DATETIME NOT NULL,
+  `LogLevel` VARCHAR(45) NOT NULL,
+  `LogText` MEDIUMTEXT NOT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE INDEX `Id_UNIQUE` (`Id` ASC),
+  INDEX `Log_INDEX` (`LogLevel` ASC, `LogDateTime` ASC),
+  FULLTEXT INDEX `LogText_FULLTEXT` (`LogText` ASC),
+  INDEX `fk_Log_ScriptInstance_idx` (`ScriptInstanceId` ASC),
+  CONSTRAINT `fk_Log_ScriptInstance`
+    FOREIGN KEY (`ScriptInstanceId`)
+    REFERENCES `EESLP`.`ScriptInstance` (`Id`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
