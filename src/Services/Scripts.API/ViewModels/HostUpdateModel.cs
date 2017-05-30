b0a151537b14956ea9718293799f5c281e406683
Andi Exer <andi.exer@windowslive.com>
2017-05-30 13:12:02 +0200

    updated validation model

--- src/Services/Scripts.API/ViewModels/HostUpdateModel.cs
+++ src/Services/Scripts.API/ViewModels/HostUpdateModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,11 @@ namespace EESLP.Services.Scripts.API.ViewModels
 {
     public class HostUpdateModel
     {
+        [Required]
         public string Hostname { get; set; }
+        [Required]
         public string FQDN { get; set; }
+        [Required]
         public string ApiKey { get; set; }
     }
 }
--- src/Services/Scripts.API/ViewModels/ScriptUpdateModel.cs
+++ src/Services/Scripts.API/ViewModels/ScriptUpdateModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace EESLP.Services.Scripts.API.ViewModels
 {
     public class ScriptUpdateModel
     {
+        [Required]
         public string Description { get; set; }
     }
 }
