d946a0d24bfdfbec664d1e6088a1dab15acb920c
Manuel Habert <manuel@habert.ch>
2017-08-16 09:56:58 +0200

    add docker functionality

--- cli-windows/build-bits.ps1
+++ cli-windows/build-bits.ps1
@@ -23,3 +23,10 @@ dotnet publish $SolutionFilePath -c Release -o .\obj\Docker\publish
 if($RemoveDockerImages) {
 	./remove-dockerimages.ps1
 }
+
+# build angular frontend
+Write-Host "Build angular 2 frontend"
+$path = pwd
+cd ..\src\Frontend\UI
+ng build --prod --aot
+cd $path
--- src/Frontend/UI/docker/setAppconfig.sh
+++ src/Frontend/UI/docker/setAppconfig.sh
@@ -2,4 +2,3 @@
 echo "{" > /var/www/assets/appconfig.json
 echo "    \"apiURI\": \""$ApiUrl"\"" >> /var/www/assets/appconfig.json
 echo "}" >> /var/www/assets/appconfig.json
-nginx -g 'daemon off;'
